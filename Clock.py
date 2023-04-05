dcf492b8322737baba2ac84bb5585540ceef14f0
Muhammad Umair Yaqub <umairyaqubofficial@gmail.com>
2023-04-05 18:39:13 +0500

    Modernization Update

--- Clock.py
+++ Clock.py
@@ -2,78 +2,86 @@ from tkinter import *
 import time
 import datetime
 from tkinter.constants import CENTER, LEFT
-from webbrowser import BackgroundBrowser
 
+# Create the main window
 Applicaton_Window = Tk()
 Applicaton_Window.config(bg='white')
 Applicaton_Window.resizable(0, 0)
 Applicaton_Window.geometry("600x350")
 Applicaton_Window.title("Clock")
-# Applicaton_Window.iconbitmap('/clock_g7g_icon.ico')
+
+# Set the initial time
 time_live = time.strftime("%H:%M:%p")
 
+# Define some style parameters
 text_font = ("Cascadia Mono", 40, "normal")
 foreground = "#737373"
 background = "white"
 user = "Umair"
 
+# Create the labels for the clock
 label_1 = Label(Applicaton_Window, font=text_font,
                 fg=foreground, bg=background, justify=LEFT)
-label_1.grid(row=1, column=1)
 label_1.pack()
 label_2 = Label(Applicaton_Window, font=text_font,
                 fg="#00c5f6", bg=background, justify=LEFT)
 label_2.pack()
 label_3 = Label(Applicaton_Window, font=text_font,
                 fg=foreground, bg=background, justify=LEFT)
-# label_3.grid(row=1)
 label_3.pack()
 label_4 = Label(Applicaton_Window, font=text_font,
                 fg=foreground, bg=background, justify=LEFT)
-# label_4.grid(row=2)
 label_4.pack()
 label_5 = Label(Applicaton_Window, font=text_font,
                 fg=foreground, bg=background, justify=CENTER)
-# label_5.grid(row=0, column=1)
 label_5.pack()
 label_6 = Label(Applicaton_Window, font=text_font,
                 fg=foreground, bg=background, justify=CENTER)
-# label_6.grid(row=0, column=1)
 label_6.pack()
 label_7 = Label(Applicaton_Window, font=text_font,
                 fg=foreground, bg=background, justify=CENTER)
-label_7.place(anchor=NW)
-# label_7.grid(row=1, column=1)
 label_7.pack()
 label_8 = Label(Applicaton_Window, font=text_font,
                 fg=foreground, bg=background, justify=CENTER)
-# label_8.grid(row=0, column=1)
 label_8.pack()
 
 
 def digital_clock():
+    # Get the current hour
     hour = int(datetime.datetime.now().hour)
     if hour >= 5 and hour < 12:
+        # Set the emoji and message for morning
         label_1.config(text="😎")
         label_2.config(
             text="I hope you'll have a great day today.\n" + user + "\n" + time_live)
+        # Update the clock every 200 milliseconds
         label_2.after(200, digital_clock)
 
     elif hour >= 12 and hour < 17:
+        # Set the emoji and message for noon
         label_1.config(text="🥤")
         label_2.config(text="A great noon\n" + user + "\n" + time_live)
+        # Update the clock every 200 milliseconds
         label_2.after(200, digital_clock)
 
     elif hour >= 17 and hour < 20:
+        # Set the emoji and message for evening
         label_1.config(text="😌")
         label_2.config(text="It's evening !\n" + user + "\n" + time_live)
+        # Update the clock every 200 milliseconds
         label_2.after(200, digital_clock)
 
     else:
+        # Set the emoji and message for night
         label_1.config(text="🥱")
         label_2.config(text="Getting Sleepy...\n" + user + "\n" + time_live)
+        # Update the clock every 200 milliseconds
         label_2.after(200, digital_clock)
 
 
+# Call the digital clock function to start the clock
 digital_clock()
+
+# Run the main loop for the window
+
 Applicaton_Window.mainloop()
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# PythonClock
+# Digital Clock
+
+This is a simple digital clock built using Python's tkinter module. It displays the current time and a message based on the current time of the day (morning, noon, evening or night).
+
+## Installation
+
+To run the clock, you'll need to have Python 3 and the tkinter module installed on your machine.
+
+You can install tkinter on Ubuntu/Debian Linux using the following command:
+
+    sudo apt-get install python3-tk
+On macOS, tkinter comes pre-installed with Python 3.
+
+## Usage
+
+To run the clock, simply execute the digital_clock.py file using Python 3:
+
+    python3 digital_clock.py
+
+## License
+
+This project is licensed under the MIT license. Feel free to use, modify and distribute this code as you please.#
